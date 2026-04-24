8f19f8f1f7241e4ffdf519108a41a67ee001ebe9
ts6708-tech <ts6708@srmist.edu.in>
2026-04-24 14:33:07 +0530

    UC1: Feet equality implementation with tests

---
+++ APP/src/test/QuantityMeasurementAppTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityMeasurementAppTest {
+
+    @Test
+    void givenSameFeetValues_whenCompared_shouldReturnTrue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    void givenDifferentFeetValues_whenCompared_shouldReturnFalse() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    void givenFeetValue_whenComparedWithNull_shouldReturnFalse() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    void givenSameReference_whenCompared_shouldReturnTrue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f1));
+    }
+
+    @Test
+    void givenFeetValue_whenComparedWithDifferentType_shouldReturnFalse() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(f1.equals("1.0"));
+    }
+}
