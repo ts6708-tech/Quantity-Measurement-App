6c75bd06cff9f659b58d361c9b1040d4bc777e3b
ts6708-tech <ts6708@srmist.edu.in>
2026-04-24 14:46:36 +0530

    UC2: Feet and Inch equality implemented

--- APP/src/test/QuantityMeasurementAppTest.java
+++ APP/src/test/QuantityMeasurementAppTest.java
@@ -1,9 +1,12 @@
 package test;
 
+import main.QuantityMeasurementApp;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityMeasurementAppTest {
+
+    // 🔹 FEET TESTS
 
     @Test
     void givenSameFeetValues_whenCompared_shouldReturnTrue() {
@@ -29,7 +32,7 @@ class QuantityMeasurementAppTest {
     }
 
     @Test
-    void givenSameReference_whenCompared_shouldReturnTrue() {
+    void givenSameFeetReference_whenCompared_shouldReturnTrue() {
         QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
 
         assertTrue(f1.equals(f1));
@@ -40,5 +43,44 @@ class QuantityMeasurementAppTest {
         QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
 
         assertFalse(f1.equals("1.0"));
+    }
+
+    // 🔥 INCH TESTS
+
+    @Test
+    void givenSameInchValues_whenCompared_shouldReturnTrue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    void givenDifferentInchValues_whenCompared_shouldReturnFalse() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(2.0);
+
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    void givenInchValue_whenComparedWithNull_shouldReturnFalse() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    void givenSameInchReference_whenCompared_shouldReturnTrue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertTrue(i1.equals(i1));
+    }
+
+    @Test
+    void givenInchValue_whenComparedWithDifferentType_shouldReturnFalse() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertFalse(i1.equals("1.0"));
     }
 }
